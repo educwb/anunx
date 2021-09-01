d0ceb53ba3cc77810942a82d3ffb191688515f4f
Eduardo Silva <educwb@gmail.com>
2021-09-01 06:41:58 -0300

    adicionando _document.js e signin.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,13 @@ import theme from '../src/theme';
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
+  // React.useEffect(() => {
+  //   // Remove the server-side injected CSS.
+  //   const jssStyles = document.querySelector('#jss-server-side');
+  //   if (jssStyles) {
+  //     jssStyles.parentElement.removeChild(jssStyles);
+  //   }
+  // }, []);
 
   return (
     <React.Fragment>
