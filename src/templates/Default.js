d5be7007769da5859bf98a4463e5bcfbd4f1d845
Eduardo Silva <educwb@gmail.com>
2021-09-01 07:06:28 -0300

    adicionando estrutura para receber Header / Body / Footer

---
+++ src/templates/Default.js
@@ -0,0 +1,12 @@
+
+const Default = ({children}) => {
+  return (
+    <div>
+      <header>Header</header>
+      {children}
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default Default
